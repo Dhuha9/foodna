b3e11aedcb94b570a790d1e57b46147d20bed9e7
Dhuha9 <dhuha9hasan@gmail.com>
2021-03-18 15:35:38 +0300

    Build: Prepare the router
    add the react-router-dom dependence to route links for each page, add the routing needed in the app.js file to mach the pathes for forwording to the exact page
    
    Closes #5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import About from './pages/About/About';
+import AvailableMeals from './pages/AvailableMeals/AvailableMeals';
+import ContactUs from './pages/ContactUs/ContactUs';
+import DonateFood from './pages/DonateFood/DonateFood';
+import Main from './pages/Main/Main';
+import RecieveFood from './pages/RecieveFood/RecieveFood';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route exact path="/about" component={About} />
+        <Route path="/meals" component={AvailableMeals} />
+        <Route path="/contact" component={ContactUs} />
+        <Route path="/donate" component={DonateFood} />
+        <Route path="/recieve/:id" component={RecieveFood} />
+      </Switch>
     </div>
   );
 }
