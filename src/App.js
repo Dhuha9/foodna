6bb2992e4bdab62c0edeaf5879ade191bfe22298
Fatima Oday <fatmaoa98@gmail.com>
2021-03-18 15:19:21 +0300

    NarrowCta added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import HeroComponent from './components/HeroComponent';
+import NarrowCta from './components/NarrowCta';
 
 function App() {
   return (
     <div >
       <HeroComponent/>
+      <NarrowCta/>
     </div>
   );
 }
