32c8a1efbeb79b130e33c33bc5fc42aedd40e49d
Tabarek Ayad <tabarek.ayad@gmail.com>
2021-03-18 14:13:11 +0300

    Build: Prepare_The_Bootstrap
    add react-bootstrap and node-sass dependencies, and scss watch and output configurations on scripts
    
    Closes #15

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import './App.scss';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button variant="primary">Primary</Button>
       </header>
     </div>
   );
