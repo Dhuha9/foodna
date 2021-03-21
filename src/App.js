c6351fa66186d5fa44d169a31ab22b67333bfa12
Dhuha9 <dhuha9hasan@gmail.com>
2021-03-21 14:38:32 +0300

    Refactor: move react routes from App.js into a separate file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
-import About from './pages/About/About';
-import AvailableMeals from './pages/AvailableMeals/AvailableMeals';
-import ContactUs from './pages/ContactUs/ContactUs';
-import DonateFood from './pages/DonateFood/DonateFood';
-import Main from './pages/Main/Main';
-import RecieveFood from './pages/RecieveFood/RecieveFood';
+import routes from './Routes';
 
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/about" component={About} />
-        <Route path="/meals" component={AvailableMeals} />
-        <Route path="/contact" component={ContactUs} />
-        <Route path="/donate" component={DonateFood} />
-        <Route path="/recieve/:id" component={RecieveFood} />
-      </Switch>
-    </div>
-  );
+  return <div className="App">{routes}</div>;
 }
 
 export default App;
