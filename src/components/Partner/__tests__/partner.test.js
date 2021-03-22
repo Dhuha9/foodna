5ce5f683b36c65ae2164579571777e362c104f3f
Abdulazeez <abdulazeezalabbasi@yahoo.com>
2021-03-22 21:33:33 +0300

    Build: Partners Component

---
+++ src/components/Partner/__tests__/partner.test.js
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+import Partner from '../Partner';
+it("renders apply now correctly", () => {
+  const tree = renderer.create(<Partner/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
