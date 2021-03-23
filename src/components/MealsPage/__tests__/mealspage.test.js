b23463c6a3e2ffe7e66e42b0497711e66ac01246
Abdulazeez <abdulazeezalabbasi@yahoo.com>
2021-03-23 21:12:20 +0300

    Build: Hook Firebase With Meals

---
+++ src/components/MealsPage/__tests__/mealspage.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+import * as React from "react";
+import * as renderer from "react-test-renderer";
+import MealsPage from '../MealsPage';
+it("renders apply now correctly", () => {
+  const tree = renderer.create(<MealsPage/>).toJSON();
+  expect(tree).toMatchSnapshot();
+});
