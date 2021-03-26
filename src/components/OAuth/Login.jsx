bf77f791c4bc5432a60296a15fb1c4f9a07b578f
Fatima Oday <fatmaoa98@gmail.com>
2021-03-27 01:50:56 +0300

    google OAuth integrated

---
+++ src/components/OAuth/Login.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+// refresh token
+import { refreshTokenSetup } from '../../utils/refreshToken.js';
+
+const clientId =
+  '161545215382-ens5ss0r0novf8hdq5dc8scabpc4v4j0.apps.googleusercontent.com';
+
+function Login() {
+  const onSuccess = (res) => {
+    console.log('Login Success: currentUser:', res.profileObj);
+    alert(
+      `Logged in successfully welcome ${res.profileObj.name} 😍. \n See console for full profile object.`
+    );
+    localStorage.setItem('currentUser', JSON.stringify(res.profileObj));
+    // var userInfo = JSON.parse(localStorage.getItem('res.profileObj')); <-- This line can be used to get the user's info from local storage
+    
+    refreshTokenSetup(res);
+  };
+
+  const onFailure = (res) => {
+    console.log('Login failed: res:', res);
+    alert(
+      `Failed to login. 😢`
+    );
+  };
+
+  return (
+    <div>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+        style={{ marginTop: '100px' }}
+        isSignedIn={true}
+      />
+    </div>
+  );
+}
+
+export default Login;
