fe3a3c37d153061278375e987901ec203d62dc81
Abdulazeez <abdulazeezalabbasi@yahoo.com>
2021-03-27 16:30:51 +0300

    Fix: lint testing

--- src/components/Partners/__tests__/partners.test.js
+++ src/components/Partners/__tests__/partners.test.js
@@ -1,7 +1,7 @@
-import * as React from "react";
-import * as renderer from "react-test-renderer";
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
 import Partners from '../Partners';
-it("renders apply now correctly", () => {
-  const tree = renderer.create(<Partners/>).toJSON();
+it('renders apply now correctly', () => {
+  const tree = renderer.create(<Partners />).toJSON();
   expect(tree).toMatchSnapshot();
 });
