a753bfdf3a2c3fb9f42c3a609e263700abfdfd43
Abdulazeez <abdulazeezalabbasi@yahoo.com>
2021-03-27 16:30:51 +0300

    Fix: lint testing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
-import './partners.css'
-
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
