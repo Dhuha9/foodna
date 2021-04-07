b7df5ea955f8a66fe045ef177e9b02ef09da7454
Tabarek Ayad <tabarek.ayad@gmail.com>
2021-04-07 15:40:28 +0300

    temp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import './App.scss';
+import DonateForm from './components/donateForm/DonateForm';
 
 function App() {
-  return <div className="App">
-    
-    </div>;
+  return (
+    <div className="App">
+      <DonateForm />
+    </div>
+  );
 }
 
 export default App;
--- src/components/donateForm/DonateForm.jsx
+++ src/components/donateForm/DonateForm.jsx
@@ -40,19 +40,21 @@ function DonateForm() {
 
   return (
     <Container>
-      <Row>
-        <Col xs lg="4">
-          <img className="mx-4 donationForm" src={M} alt="donations" />
-        </Col>
-        <Col className="px-4 donationDesc" xs lg="8">
-          <h1 className="mb-4">Donate what you have</h1>
-          <p className="donationText">
+      <Row className="text-left overflow-hidden">
+        <Col
+          className="d-none d-sm-block side-div"
+          style={{ backgroundImage: `url(${M})`, backgroundSize: 'contain' }}
+          sm="4"
+        ></Col>
+        <Col xs="12" sm="8">
+          <h1 className="mb-4">Donate your nextra food</h1>
+          <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.{' '}
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
 
           <Form onSubmit={onSubmit}>
-            <Form.Group controlId="formBasicEmail">
+            <Form.Group controlId="formBasicName">
               <Form.Label>Full Name</Form.Label>
               <Form.Control
                 name="name"
@@ -78,7 +80,7 @@ function DonateForm() {
               </Form.Text>
             </Form.Group>
 
-            <Form.Group controlId="formBasicEmail">
+            <Form.Group controlId="formBasicPhone">
               <Form.Label>Phone number</Form.Label>
               <Form.Control
                 name="phone"
@@ -91,28 +93,6 @@ function DonateForm() {
               </Form.Text>
             </Form.Group>
 
-            <Form.Group>
-              <Form.Label
-                className="my-1 mr-2"
-                htmlFor="inlineFormCustomSelectPref"
-              >
-                Food form
-              </Form.Label>
-              <Form.Control
-                name="food"
-                onChange={handleChange}
-                as="select"
-                className="my-1 mr-sm-2"
-                id="inlineFormCustomSelectPref"
-                custom
-              >
-                <option value="0">Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-              </Form.Control>
-            </Form.Group>
-
             <Form.Group controlId="exampleForm.ControlTextarea1">
               <Form.Label>Description</Form.Label>
               <Form.Control
@@ -121,6 +101,19 @@ function DonateForm() {
                 as="textarea"
                 rows={3}
               />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicQuantity">
+              <Form.Label>Quantity</Form.Label>
+              <Form.Control
+                name="name"
+                onChange={handleChange}
+                type="text"
+                placeholder="The quantity of food"
+              />
+              <Form.Text className="text-muted">
+                Please type the quantity of food.
+              </Form.Text>
             </Form.Group>
 
             <Form.Group controlId="exampleForm.ControlTextarea1">
@@ -138,7 +131,7 @@ function DonateForm() {
               <Form.File.Input name="photo" onChange={handleChange} />
             </Form.File>
 
-            <Button variant="success" type="submit">
+            <Button className="my-4" variant="success" type="submit">
               Submit
             </Button>
           </Form>
