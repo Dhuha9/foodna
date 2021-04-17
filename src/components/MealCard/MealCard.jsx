4881a0e9dd0e6bac0033c051781b3ad75c000e15
Dhuha9 <dhuha9hasan@gmail.com>
2021-04-17 21:29:48 +0300

    Refactor: add placeholder when MealCard has no content

--- src/components/MealCard/MealCard.jsx
+++ src/components/MealCard/MealCard.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../meals.css';
 import { useTranslation } from 'react-i18next';
-
+import noImage from '../../noImage.png';
 export default function MealCard({ img, title, organization }) {
   const { t } = useTranslation();
 
   return (
     <div className="card">
       <div className="image-section">
-        <img src={img} className="card-img" alt="food" />
+        <img src={img || noImage} className="card-img" alt="food" />
       </div>
       <div className="card-body">
-        <h5 className="card-title ">{title}</h5>
-        <span className="card-text">{organization}</span>
+        <h5 className="card-title ">{title || 'No Tilte Available'}</h5>
+        <span className="card-text">
+          {organization || 'No Organization Available'}
+        </span>
       </div>
       <button type="button" className="button w-100">
         {t('AvailableMealsComponent.button')}
