08aae5673caadaf192df94b559c65f012240993a
Dhuha9 <dhuha9hasan@gmail.com>
2021-04-17 15:55:41 +0300

    Feat: add Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import './App.scss';
-import routes from './Routes';
-import Footer from './components/Footer/Footer';
+import React, { createContext, useState } from 'react';
+import './App.css';
+import Routes from './Routes';
+import Header from './components/Header/Header';
 
+export const UserContext = createContext();
 function App() {
+  const getCurrentUser = () => JSON.parse(localStorage.getItem('currentUser'));
+  const [user, setuser] = useState(getCurrentUser());
+
   return (
-    <div className="App">
-      {routes}
-      <Footer />
+    <div>
+      <UserContext.Provider value={{ refresh: setuser, user: user }}>
+        <Header />
+        <Routes />
+      </UserContext.Provider>
     </div>
   );
 }
