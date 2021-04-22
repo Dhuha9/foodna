da1c64950cc07e41f6c729e6c18344f29ede64a1
Dhuha9 <dhuha9hasan@gmail.com>
2021-04-22 14:53:40 +0300

    Refactor: adjust language button margin

--- src/components/LangButton/LangButton.jsx
+++ src/components/LangButton/LangButton.jsx
@@ -1,33 +1,31 @@
-import React from 'react'
+import React from 'react';
 import { Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { useTranslation} from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 export default function LangButton() {
+  const { i18n } = useTranslation();
 
-    const { i18n } = useTranslation();
+  const changeLanguage = (lng) => {
+    i18n.changeLanguage(lng);
+  };
 
-    const changeLanguage = (lng) => {
-      i18n.changeLanguage(lng);
-    };
-  
-    const currentLang = i18n.language || window.localStorage.i18nextLng || '';
-  
-    return (
-      <div >
-        <Form>
-          <Form.Group >
-              <Form.Control 
-                as="select" 
-                size="sm" 
-                value={currentLang} onChange={() => changeLanguage(event.target.value)}
-                style={{width: `90px`}}
-                >
-                  <option value="en">English</option>
-                  <option value="ar">Arabic</option>
-              </Form.Control>
-          </Form.Group>
-        </Form>
-      </div>
-    );
+  const currentLang = i18n.language || window.localStorage.i18nextLng || '';
+
+  return (
+    <Form className="mt-3">
+      <Form.Group>
+        <Form.Control
+          as="select"
+          size="sm"
+          value={currentLang}
+          onChange={() => changeLanguage(event.target.value)}
+          style={{ width: `90px` }}
+        >
+          <option value="en">English</option>
+          <option value="ar">Arabic</option>
+        </Form.Control>
+      </Form.Group>
+    </Form>
+  );
 }
