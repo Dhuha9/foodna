88c6fb9b31f89b725064b789656959f1460ca371
Dhuha9 <dhuha9hasan@gmail.com>
2021-04-23 15:41:25 +0300

    Chore: add new firebase setup

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,18 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
-// Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  var firebaseConfig = {
-    apiKey: "AIzaSyAbpgBLNca9tYBpxGE6e1MUfEONGStnF8A",
-    authDomain: "foodna-935d5.firebaseapp.com",
-    projectId: "foodna-935d5",
-    storageBucket: "foodna-935d5.appspot.com",
-    messagingSenderId: "759058373201",
-    appId: "1:759058373201:web:1f9ed45b00423aef577670",
-    measurementId: "G-BNGDB87SR9"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
-  
-  export default firebase
+//Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: 'AIzaSyC25fztMQ4dPd7iaTD_W3Lc5UQ7R3B0pu0',
+  authDomain: 'foodna-two.firebaseapp.com',
+  projectId: 'foodna-two',
+  storageBucket: 'foodna-two.appspot.com',
+  messagingSenderId: '166893343315',
+  appId: '1:166893343315:web:a40c1fa57e69b98a5e2ca6',
+  measurementId: 'G-DX36Z1ZP6B',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+export default firebase;
