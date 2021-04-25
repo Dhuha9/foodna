efe9940795746f4b5a13551437ee5c04c59be822
Dhuha9 <dhuha9hasan@gmail.com>
2021-04-25 14:57:19 +0300

    Fix: solving Feature component style on smaller screens

--- src/components/Features/Feature.jsx
+++ src/components/Features/Feature.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Row, Col, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
+import './Features.scss';
 
 function Feature({ index, image, title, description }) {
   const { t } = useTranslation();
@@ -15,18 +16,19 @@ function Feature({ index, image, title, description }) {
           style={{ backgroundImage: `url(${image})`, backgroundSize: 'cover' }}
         ></Col>
         <Col
-          xs={{ span: 5, order: index % 2 === 0 ? 3 : 1 }}
+          xs={{ order: index % 2 === 0 ? 3 : 1 }}
           md={6}
-          className="text-left justify-items-center align-items-center vh-100 px-5  w-50"
+          className="text-left justify-items-center align-items-center px-3 px-xl-5 feature-width feature_height  d-flex justify-content-center align-items-center "
         >
-          <div className="d-flex justify-content-center align-items-center py-5 px-5 vh-100 ">
-            <div className="py-5 px-5">
-              <h1 className="pt-4 mb-4">{title}</h1>
+          <div className=" px-3 px-xl-5">
+            <div className="py-5 px-3 px-lg-5 feature-text  ">
+              <h1 className="my-4">{title}</h1>
               <p className="my-4">{description}</p>
               <Button className="mb-4" variant="outline-success" href="/about">
                 {t(`FeatureComponent.ButtonTitle`)}
               </Button>
             </div>
+            <hr className="d-block d-md-none m-0" />
           </div>
         </Col>
       </Row>
