349987b9ea707d0a7cd2a79f1dd40688fa471d8c
Dhuha9 <dhuha9hasan@gmail.com>
2021-04-25 22:58:55 +0300

    Refactor: adjust LangButton style

--- src/components/LangButton/LangButton.jsx
+++ src/components/LangButton/LangButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useTranslation } from 'react-i18next';
-
+import './LangButton.scss';
 export default function LangButton() {
   const { i18n } = useTranslation();
 
@@ -21,6 +21,7 @@ export default function LangButton() {
           value={currentLang}
           onChange={() => changeLanguage(event.target.value)}
           style={{ width: `90px` }}
+          className="langButton"
         >
           <option value="en">English</option>
           <option value="ar">Arabic</option>
